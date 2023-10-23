313849f59ef9cc048c4ea28a9f3fe0fdb8176d4a
termraider <113169630+termraider@users.noreply.github.com>
2023-10-23 11:03:21 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,74 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Notice, Plugin, Modal, Folder, TFile } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    async onload() {
+        this.addCommand({
+            id: 'delete-and-recreate-notes',
+            name: 'Delete and Recreate Notes in Folder',
+            callback: () => this.processFolder(),
+            hotkeys: []
+        });
+    }
 
-	async onload() {
-		await this.loadSettings();
+    async promptForFolderPath(): Promise<string | null> {
+        return new Promise((resolve) => {
+            class FolderPathModal extends Modal {
+                constructor(app) {
+                    super(app);
+                }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+                onOpen() {
+                    const { el } = this;
+                    const promptDiv = el.createDiv();
+                    promptDiv.setText('Enter the folder path: ');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                    const input = promptDiv.createEl('input', { type: 'text' });
+                    let folderPath = '';
+                    input.addEventListener('input', (e) => folderPath = e.target.value);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                    const submitButton = promptDiv.createEl('button');
+                    submitButton.setText('Submit');
+                    submitButton.addEventListener('click', () => {
+                        this.close();
+                        resolve(folderPath);
+                    });
+                }
+            }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+            new FolderPathModal(this.app).open();
+        });
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async processFolder() {
+        const folderPath = await this.promptForFolderPath();
+        if (!folderPath) {
+            new Notice('No path provided.');
+            return;
+        }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        const folder = this.app.vault.getAbstractFileByPath(folderPath);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        if (!folder || !(folder instanceof Folder)) {
+            new Notice('Folder not found.');
+            return;
+        }
 
-	onunload() {
+        // Get all note files in the folder.
+        const notes = folder.children.filter(file => file instanceof TFile);
 
-	}
+        // Save note names.
+        const noteNames = notes.map(note => note.basename);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+        // Delete all notes.
+        for (const note of notes) {
+            await this.app.vault.delete(note);
+        }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+        // Create new notes with saved names.
+        for (const name of noteNames) {
+            await this.app.vault.create(name + '.md', '');
+        }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Notice('Notes processed successfully.');
+    }
 }
